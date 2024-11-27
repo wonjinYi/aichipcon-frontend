3a37a2b5a5faab9c988c273f853e7f3be3a1ca90
Wonjin Lee <studioplug17@gmail.com>
2024-11-27 21:07:14 +0900

    remove: Unused files (videoFile store)

--- app/src/store.js
+++ app/src/store.js
@@ -1,6 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import videoFileSlice from "./stores/videoFileSlice";
 import frameDataReducer from "./stores/frameDataSlice";
 import frameConfigReducer from "./stores/frameConfigSlice";
 import roiDataReducer from "./stores/roiDataSlice";
@@ -8,7 +7,6 @@ import loadingReducer from "./stores/loadingSlice";
 
 const store = configureStore({
   reducer: {
-    videoFile: videoFileSlice, // 선택된 비디오 파일
     frameConfig: frameConfigReducer, // 영상 프레임 선택 범위, 현재 프레임. (start, end, current frame)
     frameData: frameDataReducer, // 인퍼런스 돌린 결과.
     roiData: roiDataReducer, // ROI(폴리곤) 아이템 목록,
