53c9cf7138b67860b7cdd336d940d360e3cd2208
Wonjin Lee <studioplug17@gmail.com>
2024-11-28 17:28:43 +0900

    temp: Disable CameraFeed component

--- app/src/App.js
+++ app/src/App.js
@@ -10,7 +10,7 @@ import RoiList from "./components/RoiList";
 import Dashboard from "./components/Dashboard";
 import RoiEditor from "./components/RoiEditor";
 import LoadingOverlay from "./components/LoadingOverlay.js";
-import CameraFeed from "./components/CameraFeed.js";
+// import CameraFeed from "./components/CameraFeed.js";
 
 function App() {
   const roiData = useSelector((state) => state.roiData); // user 슬라이스 상태
@@ -26,7 +26,7 @@ function App() {
 
   return (
     <div className="app">
-      <CameraFeed />
+      {/* <CameraFeed /> */}
       <div className="left-container">
         <div className="left-top-container">
           {frameData.length ? (
